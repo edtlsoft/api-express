4276f9e067c99be56948bb6db9081aa1f934dca0
Edward Lopez <edward.trigos@nivelics.co>
2022-01-05 23:07:33 -0500

    [add] Routing with Express.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log('My App');
+const express = require('express')
 
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hi everyone!')
+})
+
+app.get('/product', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'Product One',
+    price: 150000
+  })
+})
+
+app.listen(3000)
