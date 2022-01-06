91d335afa05f848d5c7f72ac6df446e2c3148eb4
Edward Lopez <edward.trigos@nivelics.co>
2022-01-05 23:20:05 -0500

    [add] Receiving parameters by get method

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hi everyone!')
-})
+  res.send('Hi everyone!');
+});
 
 app.get('/product', (req, res) => {
-  res.json({
-    id: 1,
-    name: 'Product One',
-    price: 150000
-  })
-})
+  res.json([
+    {
+      id: 1,
+      name: 'Product One',
+      price: 150000,
+    },
+    {
+      id: 2,
+      name: 'Product Two',
+      price: 10000,
+    },
+  ]);
+});
 
-app.listen(3000)
+app.get('/product/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: 'Product #' + id,
+    price: 10000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+app.listen(3000);
